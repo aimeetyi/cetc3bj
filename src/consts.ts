bece5c3b167b9915beca8a316fe186a31766024b
aimeetyi <50353017+aimeetyi@users.noreply.github.com>
2026-04-21 14:46:49 +0800

    更新网站标题为中国电科北京民行交叉冤案网

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "中国电科北京民行交叉冤案网";
+export const SITE_DESCRIPTION = "中国电科劳动人事争议北京民行交叉冤案维权网站，七年冤情，依法维权，匡扶正义！";
